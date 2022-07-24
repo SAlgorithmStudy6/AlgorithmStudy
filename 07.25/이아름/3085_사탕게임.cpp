e8289c491463ff08db04a15d9a177155d8d4dccd
dldkfma9577 <arlee957790@gmail.com>
2022-07-24 17:25:25 +0900

    [ADD] 7.25 스터디 3085_사탕게임 추가

--- 07.25/이아름/3085_사탕게임.cpp
+++ 07.25/이아름/3085_사탕게임.cpp
@@ -1,1 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
+using namespace std;
+
+int N, biggest = 0;
+vector<string> board;
+int px[] = { 0,-1,0,1 };
+int py[] = { -1,0,1,0 };
+void checkSame() { //연속되는 개수 세기
+	for (int i = 0; i < N; i++) {
+		int count = 1;
+		for (int k = 1; k < N; k++) { // 행확인
+			if (board[i][k - 1] == board[i][k]) {
+				count++;
+				biggest = max(count, biggest);
+			}
+			else {
+				count = 1;
+			}
+		}
+		count = 1;
+		for (int k = 1; k < N; k++) { //열확인
+			if (board[k-1][i] == board[k][i]) {
+				count++;
+				biggest = max(count, biggest);
+			}
+			else {
+				count = 1;
+			}
+		}
+	}
+}
+
+int main() {
+	//입력 시작
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		string str; cin >> str;
+		board.push_back(str);
+	}
+
+	//본 코드 시작
+	for (int i = 0; i < N; i++) {
+		for (int k = 0; k < N; k++) {
+			int nextX, nextY;
+			for (int t = 0; t < 4; t++) {
+				nextX = i + px[t];
+				nextY = k + py[t];
+				if (0 <= nextX && nextX < N && 0 <= nextY && nextY < N) {
+					swap(board[nextX][nextY], board[i][k]);
+					checkSame();
+					swap(board[nextX][nextY], board[i][k]);
+				}
+			}
+		}
+	}
+
+	cout << biggest << endl;
+	return 0;
+}
