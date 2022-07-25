4c270da2d42065f5038faf8163b748404a002507
LeeJiYoon <sundaeluv98@gmail.com>
2022-07-25 19:48:11 +0900

    07.25
    time over

--- 07.25/이지윤/[2422]한윤정이 이탈리에 가서 아이스크림을 사먹는데.py
+++ 07.25/이지윤/[2422]한윤정이 이탈리에 가서 아이스크림을 사먹는데.py
@@ -1,19 +1,18 @@
 import itertools
 
-# def solution(_n, _m, _arr) -> int :
-#     count = 0
-#     for i in  itertools.combinations(range(1, _n+1), 3):
-#         for j in _arr:
-#             set_i = set(i)
-#             if set_i - j != set_i : break
-#             else :
-#                 print(count)
-#                 print(i)
-#                 print(j)
-#                 count += 1
-#     return count
-#
-# n, m = map(int, input().split())
-# arr = [list(map(int, input().split())) for i in range(m)]
-# answer = solution(n, m, arr)
-# print(answer)
+def solution(_n, _m, _arr) -> int :
+    count = 0
+    for combs in itertools.combinations(range(1, _n+1), 3):
+        flag = 0
+        for badComb in _arr:
+            if badComb[0] in combs and badComb[1] in combs:
+                flag = 1
+                break
+        if flag: continue
+        else : count += 1
+    return count
+
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for i in range(m)]
+answer = solution(n, m, arr)
+print(answer)
