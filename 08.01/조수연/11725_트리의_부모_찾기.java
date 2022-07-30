fbd21c735afdacb565033c896070baa4aa0a5450
su6378 <josooyeon11@naver.com>
2022-07-30 19:27:39 +0900

    11725_트리의_부모_찾기 solve

--- 08.01/조수연/11725_트리의_부모_찾기.java
+++ 08.01/조수연/11725_트리의_부모_찾기.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class Main {
 
 	static int N;
-	static boolean[] visited = new boolean[100001];
-	static int[] node = new int[100001];
+	static boolean[] visited = new boolean[100001]; //중복체크
+	static int[] node = new int[100001]; //노드 배열
 	static Queue<Integer> current_floor = new LinkedList<Integer>(); 
 
 	public static void main(String[] args) throws Exception {
@@ -13,16 +13,16 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		N = Integer.parseInt(br.readLine());
-		List<Integer> pair[] = new ArrayList [N+1];
+		List<Integer> pair[] = new ArrayList [N+1]; //노드 짝 배열
 		
 		for (int i = 0; i < pair.length; i++) {
 			pair[i] = new ArrayList<Integer>(); 
 		}
 		
-		visited[1] = true;
+		visited[1] = true; 
 		node[1] = 1;
 
-		for (int i = 0; i < N-1; i++) {
+		for (int i = 0; i < N-1; i++) { 
 			StringTokenizer token = new StringTokenizer(br.readLine());
 			int pair1 = Integer.parseInt(token.nextToken());
 			int pair2 = Integer.parseInt(token.nextToken());
@@ -30,14 +30,15 @@ public class Main {
 			pair[pair1].add(pair2);
 			pair[pair2].add(pair1);
 		}
-
+	
 		current_floor.add(1);
 
 		while (!current_floor.isEmpty()) {
 			
 			int parent = current_floor.poll();
 			
-			for (Integer i : pair[parent]) {
+			for (Integer i : pair[parent]) { //짝 배열에 current_floor poll값을 가져온 것에 노드 값 삽입
+			
 				if(!visited[i]) {
 					visited[i] = true;
 					node[i] = parent;
