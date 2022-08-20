6d3b46a15acac5f37885e58cb854b14766fb8b1d
dldkfma9577 <arlee957790@gmail.com>
2022-08-20 20:38:49 +0900

    [이아름] 3번 문제 주석처리

--- 프로그래머스_모의고사/3차_문제집/이아름/3_적진침투.cpp
+++ 프로그래머스_모의고사/3차_문제집/이아름/3_적진침투.cpp
@@ -6,20 +6,19 @@
 using namespace std;
 
 int solution(int distance, vector<vector<int>> scope, vector<vector<int>> times) {
-	map<int, int> m;
+	map<int, int> m; //time을 보기위해 index 저장
 	for (int i = 0; i < scope.size(); i++) {
 		m[scope[i][0]] = i;
 	}
-	sort(scope.begin(), scope.end());
+	sort(scope.begin(), scope.end()); //위치 오름차순으로 정렬
 	for (vector<int> s : scope) {
 		int index = m[s[0]];
 		for (int i = min(s[0], s[1]); i <= max(s[0], s[1]); i++) {
-			int per = i % (times[index][0] + times[index][1]);
-			if (per != 0 && per <= times[index][0]) {
-				return i;
+			int per = i % (times[index][0] + times[index][1]); //침투하는 턴의 시간
+			if (per != 0 && per <= times[index][0]) { //시간 1 ~ 근무 : 들킴
+				return i; //중간에 들키면 해당 위치 return 
 			}
 		}
 	}
-
 	return distance;
 }
