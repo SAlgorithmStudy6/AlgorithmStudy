b9f9b7948220024c7c9c9679a9ac4efcec543351
JI YOON LEE <50359789+jiy00nLee@users.noreply.github.com>
2022-09-16 11:33:39 +0900

    Update [모의SW역량테스트]벌꿀 채취.py

--- 09.12/이지윤/[모의SW역량테스트]벌꿀 채취.py
+++ 09.12/이지윤/[모의SW역량테스트]벌꿀 채취.py
@@ -1,4 +1,4 @@
-import sys; sys.stdin = open("input.txt")
+# import sys; sys.stdin = open("input.txt")
 
 profit = 0
 def calc_max_profit(_row_index, _col_index, _col_last_index, _limit, _sum, _result) :
@@ -11,26 +11,23 @@ def calc_max_profit(_row_index, _col_index, _col_last_index, _limit, _sum, _resu
     calc_max_profit(_row_index, _col_index + 1, _col_last_index, _limit, _sum+value, _result+value**2)
     calc_max_profit(_row_index, _col_index + 1, _col_last_index, _limit, _sum, _result)
 
-def solution_diffRow(_limit) -> int:
+def solution(_limit) -> int:
     global profit
     max_profit = 0
-    for first_row_index in range(n-1):
+    for first_row_index in range(n - 1):
         for first_col_index in range(n):
-            if first_col_index + m >= n: break
-            calc_max_profit(first_row_index, first_col_index, first_col_index+m, _limit, 0, 0)
-            profit1 = profit; profit=0
-            for second_row_index in range(first_col_index + 1, n):
+            if first_col_index + m > n: break
+            calc_max_profit(first_row_index, first_col_index, first_col_index + m, _limit, 0, 0)
+            profit1 = profit
+            profit = 0
+            for second_row_index in range(first_row_index + 1, n):
                 for second_col_index in range(n):
-                    if second_col_index + m >= n: break
-                    calc_max_profit(second_row_index, second_col_index, second_col_index+m, _limit, 0, 0)
-                    profit2 = profit; profit=0
-                    max_profit = max(max_profit, profit1+profit2)
+                    if second_col_index + m > n: break
+                    calc_max_profit(second_row_index, second_col_index, second_col_index + m, _limit, 0, 0)
+                    profit2 = profit
+                    profit = 0
+                    max_profit = max(max_profit, profit1 + profit2)
     return max_profit
-
-def solution(_limit) -> int:
-    profit_diff = solution_diffRow(_limit)
-    profit_same = 0
-    return max(profit_diff, profit_same)
 
 T = int(input())
 for t in range(1,T+1):
