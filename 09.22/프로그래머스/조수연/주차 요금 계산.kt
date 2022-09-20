edd185e33314cc39aadd3b200b9b23522d7840ce
조수연 <48742378+su6378@users.noreply.github.com>
2022-09-20 14:37:07 +0900

    update

--- 09.22/프로그래머스/조수연/주차 요금 계산.kt
+++ 09.22/프로그래머스/조수연/주차 요금 계산.kt
@@ -1,71 +1,51 @@
 package com.ssafy.algorithm
 
 import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
-import kotlin.math.ceil
 
 fun main() {
     val sl = Solution()
-    val fees = intArrayOf(180, 5000, 10, 600)
-    val records = arrayOf(
-        "05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"
+    val record = arrayOf(
+        "Enter uid1234 Muzi",
+        "Enter uid4567 Prodo",
+        "Leave uid1234",
+        "Enter uid1234 Prodo",
+        "Change uid4567 Ryan"
     )
-    println(sl.solution(fees, records))
+    println(sl.solution(record))
 }
 
 class Solution {
-    fun solution(fees: IntArray, records: Array<String>): IntArray {
-        val queue: Queue<String> = LinkedList() //주차 차량 입출차 확인 큐
-        val map: HashMap<String, Int> = HashMap() //해당 차량정보
-        val parkTime: HashMap<String, Int> = HashMap() //차량 정보
-
-        for (record in records) {
-            val information = record.split(" ")
-            if (information[2] == "IN") { //입차
-                queue.add(information[1])
-                val time = information[0].split(":")
-                val hour = (time[0].toInt()) * 60
-                val minute = time[1].toInt()
-                map.put(information[1], hour + minute)
-            } else { //출차
-                queue.remove(information[1])
-                val time = information[0].split(":")
-                val hour = (time[0].toInt()) * 60
-                val minute = time[1].toInt()
-
-                val pTime = (hour + minute) - map.getValue(information[1]) // 주차시간
-                if (parkTime.containsKey(information[1])) {
-                    parkTime.put(information[1], parkTime.getValue(information[1]) + pTime) //주차시간 parkTime에 갱신
-                } else {
-                    parkTime.put(information[1], pTime)
+    fun solution(record: Array<String>): Array<String> {
+        val user = HashMap<String, String>() // <id,nickname> map
+        val command = ArrayList<Pair<Int,String>>() // <command,id> map
+        for (info in record) { //채팅방 메세지 세팅
+            val information = info.split(" ")
+            when (information[0]) { //첫 단어로 구별
+                "Enter" -> {
+                    user.put(information[1], information[2]) //user 갱신
+                    command.add(Pair(1,information[1])) //명령어 삽입
+                }
+                "Leave" -> {
+                    command.add(Pair(2,information[1]))
+                }
+                "Change" -> {
+                    user.put(information[1], information[2])
                 }
             }
         }
 
-        if (queue.isNotEmpty()) { //IN만 하고 OUT 안 한 상황
-            while (!queue.isEmpty()) {
-                val key = queue.poll()
-                val pTime = (60 * 23) + 59 - map.getValue(key)
-                if (parkTime.containsKey(key)){ //들어온 적이 있으면
-                    parkTime.put(key, parkTime.getValue(key) + pTime) //23:59에 출차하는 것으로 간주
-                }else{
-                    parkTime.put(key,pTime)
-                }
-            }
-        }
-
-        val sortedMap = parkTime.toSortedMap()
-        val answer = IntArray(sortedMap.size)
+        val answer = Array(command.size){""}
         var index = 0
-
-
-        for (info in sortedMap) {
-            if (info.value - fees[0] >= 0){ //기본시간 보다 클 경우
-                answer[index] = fees[1] + ceil((info.value - fees[0]).toDouble() / fees[2].toDouble()).toInt() * fees[3] //요금 정산
-            }else answer[index] = fees[1]
+        for (pair in command){
+            if (pair.first == 1){ // 1이면 enter 2이면 leave
+                answer[index] = user.getValue(pair.second) + "님이 들어왔습니다."
+            }else{
+                answer[index] = user.getValue(pair.second) + "님이 나갔습니다."
+            }
             index++
         }
-
         return answer
     }
 }
