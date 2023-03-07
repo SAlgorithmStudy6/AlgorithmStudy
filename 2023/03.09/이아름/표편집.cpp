24557c5c8a12818486abbce302aa2fc2f3cb6ad6
이아름 <72062916+Lee-Areum@users.noreply.github.com>
2023-03-07 23:39:30 +0900

    Update 표편집.cpp

--- 2023/03.09/이아름/표편집.cpp
+++ 2023/03.09/이아름/표편집.cpp
@@ -18,7 +18,7 @@ string solution(int n, int k, vector<string> cmd) {
         list.push_back({ i-1, i, i + 1 });
     }
     int selected = k;
-    stack<Node> st;
+    stack<int> st;
     for (string str : cmd) {
         if (str[0] == 'U' || str[0] == 'D') {
             int x = stoi(str.substr(2));
@@ -34,7 +34,7 @@ string solution(int n, int k, vector<string> cmd) {
         else if (str[0] == 'C') {
             int l = list[selected].prev;
             int r = list[selected].next;
-            st.push(list[selected]);
+            st.push(selected);
             answer[selected] = 'X';
             if (l >= 0) {
                 list[l].next = r;
@@ -50,16 +50,16 @@ string solution(int n, int k, vector<string> cmd) {
             }
         }
         else {
-            Node num = st.top(); st.pop();
-            int l = num.prev;
-            int r = num.next;
+            int num = st.top(); st.pop();
+            int l = list[num].prev;
+            int r = list[num].next;
             if (l >= 0) {
-                list[l].next = num.val;
+                list[l].next = num;
             }
             if (r < n) {
-                list[r].prev = num.val;
+                list[r].prev = num;
             }
-            answer[num.val] = 'O';
+            answer[num] = 'O';
         }
     }
     return answer;
