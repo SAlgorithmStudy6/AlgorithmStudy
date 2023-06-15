e7381c248697cc9912e521f73e46dc22e9f1990a
HoJoJeong <43957736+hojoJeong@users.noreply.github.com>
2023-06-15 17:27:41 +0900

    Create 합분해.java

---
+++ 2023/06.15/정호조/합분해.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 합분해_2225 {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[K + 1][N + 1];
+
+        for (int i = 1; i <= K; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= K; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000000;
+            }
+        }
+
+        System.out.println(dp[K][N]);
+    }
+}
